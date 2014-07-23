fe668c6d2e298016f26a05767d778299a6718f84
Tyler Tadej <tylertadej@users.noreply.github.com>
2014-07-22 18:01:34 -0700

    CSS consolidation: *Removed Colors stylesheet *Added Variables file from Vanilla static *Refactored main.scss

---
+++ app/scripts/controllers/style-guide.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name volusionMethodThemeApp.controller:StyleGuideCtrl
+ * @description
+ * # StyleGuideCtrl
+ * Controller of the volusionMethodThemeApp
+ */
+angular.module('volusionMethodThemeApp')
+  .controller('StyleGuideCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
