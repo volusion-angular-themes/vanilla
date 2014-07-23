e04ddb71444665569bf60b916b5cf04dabdb3175
Tyler Tadej <tylertadej@users.noreply.github.com>
2014-07-22 18:01:34 -0700

    CSS consolidation: *Removed Colors stylesheet *Added Variables file from Vanilla static *Refactored main.scss

--- app/scripts/controllers/style-guide.js
+++ app/scripts/controllers/style-guide.js
@@ -7,7 +7,11 @@
  * # StyleGuideCtrl
  * Controller of the volusionMethodThemeApp
  */
+<<<<<<< HEAD
 angular.module('Volusion.controllers')
+=======
+angular.module('volusionMethodThemeApp')
+>>>>>>> CSS consolidation:
   .controller('StyleGuideCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
--- test/spec/controllers/style-guide.js
+++ test/spec/controllers/style-guide.js
@@ -3,7 +3,11 @@
 describe('Controller: StyleGuideCtrl', function () {
 
   // load the controller's module
+<<<<<<< HEAD
   beforeEach(module('methodApp'));
+=======
+  beforeEach(module('volusionMethodThemeApp'));
+>>>>>>> CSS consolidation:
 
   var StyleGuideCtrl,
     scope;
@@ -16,7 +20,11 @@ describe('Controller: StyleGuideCtrl', function () {
     });
   }));
 
+<<<<<<< HEAD
   xit('should attach a list of awesomeThings to the scope', function () {
+=======
+  it('should attach a list of awesomeThings to the scope', function () {
+>>>>>>> CSS consolidation:
     expect(scope.awesomeThings.length).toBe(3);
   });
 });
