4a6f517aec2eb8333ac20c0da751172a3f63c919
Matt Hippely <hippely@gmail.com>
2014-08-21 14:08:30 -0700

    Fix style guide test.

--- test/spec/controllers/style-guide.js
+++ test/spec/controllers/style-guide.js
@@ -1,8 +1,6 @@
 'use strict';
 
 describe('Controller: StyleGuideCtrl', function () {
-
-<<<<<<< HEAD
 	// load the controller's module
 	beforeEach(module('methodApp'));
 
@@ -20,23 +18,4 @@ describe('Controller: StyleGuideCtrl', function () {
 	xit('should attach a list of awesomeThings to the scope', function () {
 		expect(scope.awesomeThings.length).toBe(3);
 	});
-=======
-  // load the controller's module
-  beforeEach(module('methodApp'));
-
-  var StyleGuideCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    StyleGuideCtrl = $controller('StyleGuideCtrl', {
-      $scope: scope
-    });
-  }));
-
-  xit('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
->>>>>>> CSS consolidation:
 });
