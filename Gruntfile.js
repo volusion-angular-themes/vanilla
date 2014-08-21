8928004b29d45dff1528f03f5bc3241d6759385d
Matt Hippely <hippely@gmail.com>
2014-08-20 17:04:15 -0700

    Admin Tasks: * Rebase upstream/master to local branch * Fix grunt file issues post rebase * fix js issues post rebase * it builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -551,7 +551,7 @@ module.exports = function(grunt) {
 		grunt.task.run(['serve:' + target]);
 	});
 
-	grunt.registerTask('test', function() {
+	grunt.registerTask('test', function(target) {
 		grunt.task.run([
 			'clean:server',
 			'compass:server',
